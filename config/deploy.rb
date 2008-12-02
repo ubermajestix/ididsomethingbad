fe87d3992f44f40ee52979e6757cad702afff4ea
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2008-12-01 17:40:57 -0700

    deployment stuff

---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+set :application, "ididsomethingbad"
+
+#SCM settings
+set :repository,  "git@github.com:ubermajestix/ididsomethingbad.git"
+set :scm, "git"
+set :branch, "origin/master"
+# set :repository_cache, "git_cache"
+ set :deploy_via, :remote_cache
+
+
+# Sudoer settings      
+set :use_sudo               , true
+set :user                   , "tyler"
+set :runner                 , "root"
+default_run_options[:shell] = false
+default_run_options[:pty]   = true
+set :ssh_options, { :forward_agent => true }
+set :chmod755, "app config db lib public vendor script script/* public/disp*"
+
+# Deployment servers
+role :app, "ididsomethingbad.com"
+role :web, "ididsomethingbad.com"
+role :db,  "ididsomethingbad.com", :primary => true
+set :deploy_to, "/home/tyler/apps/deployed/#{application}"
+
+
+#	Restart Passenger(mod_rails)
+namespace :passenger do
+  desc "Restart Application"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+after :deploy, "passenger:restart"
